a2e297c0410b5df21df57f062df3671dbb05fda3
SUN DAS <das071832@gmail.com>
2026-07-31 16:58:29 +0530

    Time: 5 ms (53.36%), Space: 43.4 MB (91.65%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,1 @@
+class Solution { public int minOperations(String s) { int mismatches = 0; for (int i = 0; i < s.length(); i++) { char expected = (i % 2 == 0) ? '0' : '1'; if (s.charAt(i) != expected) mismatches++; } return Math.min(mismatches, s.length() - mismatches); } }
