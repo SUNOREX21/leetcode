1ed01d24fa4d9358af0d90340abc81faee7a0584
SUN DAS <das071832@gmail.com>
2026-08-14 22:14:59 +0530

    Time: 3 ms (95.92%), Space: 161 MB (90.41%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,17 +1,16 @@
 class Solution {
     public int countSubmatrices(int[][] grid, int k) {
-    int m = grid.length, n = grid[0].length;
-    int[][] prefix = new int[m + 1][n + 1];
-    int count = 0;
-    for (int i = 1; i <= m; i++) {
-    for (int j = 1; j <= n; j++) {
-    prefix[i][j] = grid[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
-    if (prefix[i][j] <= k) {
-    count++;
-
-    }
-    }
-    }
-    return count;
+        int m = grid.length, n = grid[0].length;
+        int[] cols = new int[n];   // cols[j] = sum of column j down to current row
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            int rowRunning = 0;    // running sum across this row of the column-sums
+            for (int j = 0; j < n; j++) {
+                cols[j] += grid[i][j];
+                rowRunning += cols[j];   // = sum of submatrix (0,0)..(i,j)
+                if (rowRunning <= k) count++;
+            }
+        }
+        return count;
     }
 }
